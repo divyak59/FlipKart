a0e7a67b517e864d5764ceeea787ef60ab8754f0
Divya Karri <divya.karri@dxc.com>
2020-09-28 15:08:11 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Flipkart.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
